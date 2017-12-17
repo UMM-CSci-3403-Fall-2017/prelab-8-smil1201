b3cbeac26420a8ef051685511093d452d011c10b
smil1201 <mill5978@morris.umn.edu>
2017-12-16 22:37:53 -0600

    Cleaned and commented code.

--- src/search/ThreadedSearch.java
+++ src/search/ThreadedSearch.java
@@ -27,29 +27,19 @@ public class ThreadedSearch<T> implements Runnable {
   * You can assume that the list size is divisible by `numThreads`
   */
   public boolean parSearch(int numThreads, T target, ArrayList<T> list) throws InterruptedException {
-    /*
-    * First construct an instance of the `Answer` inner class. This will
-    * be how the threads you're about to create will "communicate". They
-    * will all have access to this one shared instance of `Answer`, where
-    * they can update the `answer` field inside that instance.
-	*/
+    
+	//create an instance of the `Answer` inner class, initialize as false
 	Answer globalAnswer = new Answer();
 	globalAnswer.setAnswer(false);
-/*
-    *
-    * Then construct `numThreads` instances of this class (`ThreadedSearch`)
-    * using the 5 argument constructor for the class. You'll hand each of
-    * them the same `target`, `list`, and `answer`. What will be different
-    * about each instance is their `begin` and `end` values, which you'll
-    * use to give each instance the "job" of searching a different segment
-    * of the list. If, for example, the list has length 100 and you have
-    * 4 threads, you would give the four threads the ranges [0, 25), [25, 50),
-    * [50, 75), and [75, 100) as their sections to search.
-*/
+
+	
+	//create array list to hold ThreadedSearch runnables
 	ArrayList<ThreadedSearch> TSArray = new ArrayList<ThreadedSearch>();
 	
+	//find desired size for each section
 	int splitSize = list.size()/numThreads;
 	
+	//construct `numThreads` instances of `ThreadedSearch`
 	for(int i=0;i<numThreads; i++){
 		int start = (splitSize*i);
 		int finish = (splitSize*(i+1));
@@ -57,30 +47,27 @@ public class ThreadedSearch<T> implements Runnable {
 		TSArray.add(ts);
 	}
 	
-  /*  *
-    * You then construct `numThreads`, each of which is given a different
-    * instance of this class as its `Runnable`. Then start each of those
-    * threads, wait for them to all terminate, and then return the answer
-    * in the shared `Answer` instance.
-    */
-	
-	   Thread[] threads = new Thread[numThreads];
-	   for (int i=0; i<numThreads; ++i) {
-	      threads[i] = new Thread(TSArray.get(i));
-	      threads[i].start();
-	   }
 
-	   // Wait for all the threads to finish
-	   for (int i=0; i<numThreads; ++i) {
-	      threads[i].join();
-	   }
+   //create desired number of threads
+   Thread[] threads = new Thread[numThreads];
+   for (int i=0; i<numThreads; ++i) {
+	  // Create thread for each runnable and start them
+      threads[i] = new Thread(TSArray.get(i));
+      threads[i].start();
+   }
 
-	   // return the answer in the shared `Answer` instance
-	   return globalAnswer.getAnswer();
+   // wait for thread completion
+   for (int i=0; i<numThreads; ++i) {
+      threads[i].join();
+   }
+
+   // return the answer in the shared `Answer` instance
+   return globalAnswer.getAnswer();
 
   }
 
 public void run() {
+	//set answer to true if the target is found in the section
 	for(int i=0; i<list.size(); i++){
         if(list.get(i).equals(target)) {
         	this.answer.setAnswer(true);
